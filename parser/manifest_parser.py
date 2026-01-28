0e6cd5394caf8e6a31958a9dacadd3995efe38cc
Francisco <farenas9134@gmail.com>
2026-01-27 21:10:07 -0600

    added error catching

--- parser/manifest_parser.py
+++ parser/manifest_parser.py
@@ -5,16 +5,22 @@ import json
 
 def analyzeManifest(manifest, extClass):
     # Open the manifest.json file in read mode
-    with open(manifest, 'r') as manifest_file:
-        # Parse the JSON-formatted string into a Python dictionary
-        manifest_data = json.load(manifest_file)
-        extClass.name = manifest_data.get('name')
-        print(extClass.name)
-        extClass.permissions = manifest_data.get('permissions')
-        extClass.version = manifest_data.get('version')
-        extClass.host_permissions = manifest_data.get('host_permissions')
-        if 'content_security_policy' in manifest_data:
-            extClass.security_policy = True
+    try:
+        with open(manifest, 'r') as manifest_file:
+            # Parse the JSON-formatted string into a Python dictionary
+            manifest_data = json.load(manifest_file)
+            extClass.name = manifest_data.get('name')
+            print(extClass.name)
+            extClass.permissions = manifest_data.get('permissions')
+            extClass.version = manifest_data.get('version')
+            extClass.host_permissions = manifest_data.get('host_permissions')
+            if 'content_security_policy' in manifest_data:
+                extClass.security_policy = True
+    except FileNotFoundError:
+        print(f"Error: The file {manifest} was not found.")
+    except Exception as e:
+        print(f"An error ocurred: {e}")
+
 
 def getExtensionName(manifest, extClass):
     # Open the manifest.json file in read mode
