d276fb21d880eef04e3cfae2f0241c780b70acfb
Francisco <farenas9134@gmail.com>
2026-02-11 10:57:50 -0600

    finsihed all M ids

--- manager_old.py
+++ manager_old.py
@@ -38,7 +38,7 @@ if __name__ == "__main__":
 
         ## ONLY DO BATCHES OF 200
         # 1200, SO 6 BATCHES
-        for ext_id in benign_ext_csv_df["ID"][0:500]:
+        for ext_id in benign_ext_csv_df["ID"][:]:
             download_crx(ext_id)
         
 
